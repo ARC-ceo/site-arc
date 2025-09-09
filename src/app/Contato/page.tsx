b95041d58fb009888cc1584d962d78f21a7e9432
Rciteli <rciteli@gmail.com>
2025-09-09 20:36:20 -0300

    correcao portfolio

--- src/app/Contato/page.tsx
+++ src/app/Contato/page.tsx
@@ -246,7 +246,7 @@ function InfoCard() {
           value="/company/arc-grupo"
           href="https://www.linkedin.com/company/arc-grupo"
         />
-        <InfoRow label="Portfólio" value="site-arc.vercel.app" href="https://site-arc.vercel.app" />
+        <InfoRow label="Portfólio" value="site-arc.vercel.app" href="https://site.grouparc.com.br" />
       </div>
       <div className="mt-6 rounded-xl bg-gradient-to-br from-[#00C0FF]/20 to-transparent p-[1px]">
         <div className="rounded-xl bg-[#0C0F1A] p-4">
